9fbd29d2851d4ae329a748ba14eb7d8c927959ce
Sushant Bailkar <sushantbailkar2504@gmail.com>
2023-09-24 18:00:58 +0530

    minor changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import "./about.css";
 
 const About = () => {
   return (
@@ -7,7 +6,9 @@ const About = () => {
       className="h-[100vh] flex items-center justify-center flex-col py-4 bg-violet-100"
       id="about"
     >
-      <h1 className="text-2xl border-b-4 border-solid border-purple-950 py-1">About Me</h1>
+      <h1 className="text-2xl border-b-4 border-solid border-purple-950 py-1">
+        About Me
+      </h1>
 
       <p className="  px-4 lg:px-52 text-xl mt-4 text-purple-950">
         I'm Sushant, a passionate and dedicated Fullstack software developer
